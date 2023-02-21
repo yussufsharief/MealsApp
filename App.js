da55021b4b337736dc57b43395646a9ad2bd00e5
yussufsharief <yussufsharief@gmail.com>
2023-02-21 16:57:14 +0530

    Commit changeson React native Drawer

--- App.js
+++ App.js
@@ -5,8 +5,33 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealsDetailsScreen from "./screens/MealsDetailsScreen";
-const Stack = createNativeStackNavigator();
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import FavoritesScreen from "./screens/FavoritesScreen";
+import {Ionicons} from '@expo/vector-icons';
 
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator  screenOptions={{
+      headerStyle: { backgroundColor: "#341401" },
+      headerTintColor: "white",
+      sceneContainerStyle: { backgroundColor: "#3f2f55" },
+      drawerContentStyle:{backgroundColor:'teal'},
+      drawerActiveTintColor:'white',
+      drawerActiveBackgroundColor:'#3fcf65'
+    }}>
+      <Drawer.Screen name="Categories" component={CategoriesScreen} options={{
+              title: "All Categories",
+              drawerIcon:({color,size})=><Ionicons name="list" color={color} size={size}/>
+            }}/>
+      <Drawer.Screen name="favorites" component={FavoritesScreen} options={{
+              title: "Favorites",
+              drawerIcon:({color,size})=><Ionicons name="star" color={color} size={size}/>
+            }}/>
+    </Drawer.Navigator>
+  );
+}
 export default function App() {
   return (
     <>
@@ -21,15 +46,16 @@ export default function App() {
         >
           <Stack.Screen
             name="Meals Categories"
-            component={CategoriesScreen}
+            component={DrawerNavigator}
             options={{
               title: "All Categories",
+              headerShown:false
               // headerStyle:{ backgroundColor: "#341401" }, // we can add this on navigator to apply style for all screens
               // headerTintColor: "white",
               // contentStyle:{backgroundColor:"#3f2f55"}
             }}
           />
-        
+
           <Stack.Screen
             name="Meals Overview"
             component={MealsOverviewScreen}
@@ -41,11 +67,13 @@ export default function App() {
             // }}
           />
 
-        <Stack.Screen name="Meals Detail Screen" component={MealsDetailsScreen} 
-          // options={{headerRight:()=>{
-          // return <Button title="Tap Me!" onPress={}/> here if you want to interact with this screen component , we have toright this options on the screen instead of here
-         //}}}
-        />
+          <Stack.Screen
+            name="Meals Detail Screen"
+            component={MealsDetailsScreen}
+            // options={{headerRight:()=>{
+            // return <Button title="Tap Me!" onPress={}/> here if you want to interact with this screen component , we have toright this options on the screen instead of here
+            //}}}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+function FavoritesScreen(){
+    return <View>
+        <Text>Favorties</Text>
+    </View>
+}
+
+export default FavoritesScreen;
